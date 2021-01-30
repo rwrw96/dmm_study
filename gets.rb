e55990ab8d6f694d00e917f95c4d98a7925f3221
watabe <kazuki137@gmail.com>
2021-01-31 07:45:56 +0900

    繰り返し文の作成

---
+++ gets.rb
@@ -0,0 +1,16 @@
+puts "何回計算しますか？"
+n = gets.to_i
+k = 0
+for i in 1..n do
+    puts "数値を二つ入力してください"
+    puts "#{i} 回目の計算"
+    a = gets.to_i
+    b = gets.to_i
+    puts "a + b = #{a + b}"
+    puts "a - b = #{a - b}"
+    puts "a * b = #{a * b}"
+    puts "a / b = #{a / b}"
+end
+puts "計算を終了致します"
+
+
